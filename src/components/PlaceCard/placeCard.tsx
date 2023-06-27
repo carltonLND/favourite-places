b03a2912b41ff35defc6280fa54e44ebd56771c0
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-27 16:33:27 +0100

    Added PlaceCard component

--- src/components/PlaceCard/placeCard.tsx
+++ src/components/PlaceCard/placeCard.tsx
@@ -1,18 +1,29 @@
 import "./placeCard.css";
 
-interface PlaceCardProps {
+export interface PlaceCardProps {
   img: string;
-  // placeName: string;
-  // location: string;
-  // country: string;
-  // mapLink: string;
-  // description: string;
+  title: string;
+  placeName: string;
+  countryName: string;
+  mapLink: string;
+  description: string;
 }
 
 export function PlaceCard(props: PlaceCardProps) {
   return (
     <div className="container">
       <img className="place-img" src={props.img} alt="{props.placeName} img" />
+      <h2 className="place-title">{props.title}</h2>
+      <h4 className="place-info">
+        {props.placeName}, {props.countryName} (
+        <a className="map-link" href={props.mapLink}>
+          map link
+        </a>
+        )
+      </h4>
+      <div className="place-desc">
+        <p>{props.description}</p>
+      </div>
     </div>
   );
 }
