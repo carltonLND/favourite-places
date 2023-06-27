a367c48c40aeaced31937ce90934f16957e44ca6
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-27 16:02:50 +0100

    Added test landsEnd

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from "../Header/header";
+import { PlaceCard } from "../PlaceCard/placeCard";
 import "./App.css";
+
+const landsEnd = {
+  img: "https://images.unsplash.com/photo-1603578212992-114102235cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+};
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PlaceCard {...landsEnd} />
     </div>
   );
 }
